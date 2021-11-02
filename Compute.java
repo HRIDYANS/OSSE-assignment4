340c8829fee023f873200669b5d95bb3ecd60a18
Dileepkumar Mukundan <60811093+kumarayil@users.noreply.github.com>
2021-11-02 10:54:49 +0530

    Update Compute.java
    code indentation changes

--- Compute.java
+++ Compute.java
@@ -22,25 +22,17 @@ class PerimeterOfCircle
       
    public static void main(String args[]) 
     {   
-           Scanner s= new Scanner(System.in);
-        
+           Scanner s= new Scanner(System.in);        
            System.out.println("Enter the radius:");
-           
-	   double r= s.nextDouble();
-         
-	   double  a=PerimeterOfCircle.area(r); 
- 
+           double r= s.nextDouble();         
+	   double  a=PerimeterOfCircle.area(r);  
            System.out.println("Perimeter of Circle is: " +c);   
       }
      public static double area(double r)
-    {   
-       
-	   double  a=(22*2*r)/7;
- 
+    {          
+	   double  a=(22*2*r)/7; 
            return a;
-      }
- 
- 
+      } 
 }
 
 // added by Sanu Mony on 2nd Nov 2021
@@ -48,40 +40,28 @@ class PerimeterOfCircle
 
 class AreaOfRectangle 
 {
-   public static void main(String args[]) 
- 
-    {   
- 
-    System.out.println("The value of length:");
- 
-         double l= 5;
-         
-     System.out.println("The value of Breadth:");
-        
-         double b= 10;
-         
+   public static void main(String args[])  
+    {    
+    System.out.println("The value of length:"); 
+         double l= 5;         
+     System.out.println("The value of Breadth:");        
+        double b= 10;         
         double  area=l*b;
-     System.out.println("Area of Rectangle is: " + area);   
-   
+     System.out.println("Area of Rectangle is: " + area);      
          }
 }
 
 // added by Hridya on 2nd Nov 2021
 // new class AreaOfTriangle
-
 class AreaOfTriangle 
 {
    public static void main(String args[]) 
-    {   
-       
-      Scanner s1= new Scanner(System.in);
-        
+    {          
+      Scanner s1= new Scanner(System.in);        
          System.out.println("Enter the 1st side:");
-         int a= s1.nextInt();
- 
+         int a= s1.nextInt(); 
          System.out.println("Enter the 2nd side:");
-          int b= s1.nextInt();
- 
+          int b= s1.nextInt(); 
          System.out.println("Enter the 3rd side:");
          int c= s1.nextInt();
         if((a+b)>c && (a+c)>b && (b+c)>a)
